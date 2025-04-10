529cbd4a0edd3e6346ebc76c67ca9f4e034356e4
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-10 15:49:38 +0200

    Installation du back-end et  Mise en place du SignIn

--- Frontend/src/components/page/SignIn.jsx
+++ Frontend/src/components/page/SignIn.jsx
@@ -1,7 +1,37 @@
+import { useRef } from "react";
+
 import Footer from "../structures/Footer";
 import Header from "../structures/Header";
 
 export default function SignIn() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function Login(e) {
+    e.preventDefault();
+
+    const login = {
+      email: emailRef.current.value,  // correspond à ce que ton backend attend
+      password: passwordRef.current.value,
+    };
+
+    const response = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(login),
+    });
+
+    if (!response.ok) {
+      alert("Email/Mot de passe incorrect. Veuillez réessayer ")
+    }
+    const data = await response.json();
+    window.location.href = "./user";
+    console.log(data);
+  }
+
   return (
     <>
       <Header />
@@ -9,25 +39,22 @@ export default function SignIn() {
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form>
+          <form onSubmit={Login}>
             <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <label htmlFor="email">Username</label>
+              <input type="email" id="email" ref={emailRef} />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input type="password" id="password" ref={passwordRef} />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            {/* PLACEHOLDER DUE TO STATIC SITE */}
-            <a href="./user.html" className="sign-in-button">
+            <button type="submit" className="sign-in-button">
               Sign In
-            </a>
-            {/* SHOULD BE THE BUTTON BELOW */}
-            {/* <button className="sign-in-button">Sign In</button> */}
+            </button>
           </form>
         </section>
       </main>
