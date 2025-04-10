88c6581f3b850823ff90675e50d1d0d6c3d07e1c
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-10 10:46:19 +0200

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque vitae saepe rerum laboriosam quasi doloremque. Unde quasi sed nesciunt saepe fuga odio. Quis consectetur cupiditate deleniti doloribus dolor alias ipsa.</p>
+    </div>
+  )
+}
+
+export default App
